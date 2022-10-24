627f9af1de0478be98fd03da11c8dfde118c7c0a
Derek Ney <deek311@gmail.com>
2022-10-24 16:23:03 -0600

    erased error

--- src/routes/aggregator/+page.svelte
+++ src/routes/aggregator/+page.svelte
@@ -1,36 +1,39 @@
 <script>
-  import sanitizeHtml from "sanitize-html";
-  export let data, errors
+  import sanitizeHtml from "sanitize-html" //changing html to text
+  export let data
   $: console.log(data.topStories)
 
-  let storyContent = ''
+  let newsContent = ''
 
   function getStory(story) {
-    storyContent = sanitizeHtml(story.text)
-  }
+   newsContent = sanitizeHtml(story.text)
+
+}
 </script>
 
 <div class="drawer">
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col items-center justify-center">
-      <!-- Page content here -->
-      <label for="my-drawer-2" class="btn btn-primary drawer-button">Open drawer</label>
-      <div class="hero bg-base-200">
-        <p>{@html storyContent}</p>
+      <!-- Page content goes here -->
+      <label for="my-drawer-2" class="btn btn-primary drawer-button">Open</label>
+      <div class="hero bg-base-300">
+        <p>{@html newsContent}</p>
+
       </div>
+    
     </div> 
     <div class="drawer-side">
       <label for="my-drawer-2" class="drawer-overlay"></label> 
       <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
         <!-- Sidebar content here -->
-      {#each data.topStories as story }
+    {#each data.topStories as story }
         <li class="card" on:click={getStory(story)}>
           <figure class="flex flex-col">
-            <!-- <img src={story.image_url} alt="Story pic"/> -->
+       <!--  <img src={story.image_url} alt="news pic"/> -->
             <figcaption>{story.title}</figcaption>
           </figure>
         </li>
-        {/each}
+     {/each}
       </ul>
     
     </div>

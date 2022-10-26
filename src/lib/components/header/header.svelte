93e191f5f98931c5286c356ff31c63f66ba1e5a0
Derek Ney <deek311@gmail.com>
2022-10-26 12:10:11 -0600

    quick fix tabindex

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -31,11 +31,13 @@ function logout() {
     </form>
  </div>
  <div class="dropdown dropdown-end">
-    <label tabindex="0" class="btn btn-ghost btn-circle avatar m-1" for="userIcon">
+    <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+    <label tabindex="0" class="btn btn-ghost btn-circle avatar m-1" for="icon">
         <div class="w-16 rounded-full">
-            <img id="userIcon" src="https://placeimg.com/90/90/people" alt="User icon" />
+            <img id="icon" src="https://placeimg.com/90/90/people" alt="User icon" />
         </div>
     </label>
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
     <ul
         tabindex="0"
         class="mt-3 p-2 dropdown-content bg-base-100 menu menu-compact shadow rounded-box w-36"

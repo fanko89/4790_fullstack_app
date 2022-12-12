bba5b248b6d81a415f73b13cb43594617076ec5f
derek ney <deek311@gmail.com>
2022-12-12 15:51:39 -0700

    testing source map

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
+/**@type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {sourcemap: true}
 };
 
 export default config;

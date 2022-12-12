2ab6e3971ad2d0b267581f59dceb7e5dad98b172
derek ney <deek311@gmail.com>
2022-12-12 16:03:16 -0700

    commit

--- src/routes/avatars/+page.js
+++ src/routes/avatars/+page.js
@@ -5,7 +5,7 @@ export async function load(){
     const fiftyNames = [...Array(50)].map(() => {   //... is using the spread operator
         const lastName = faker.name.lastName()
         return {
-            lastName,  //when the name of the key and property is the same you only need to put one
+            lastName,  //when the name of the key and property is the same you only need to put one yep
             avatar:`https://avatars.dicebear.com/api/big-smile/${lastName}.svg`
         }
     })

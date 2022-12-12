54fe8770514e765271e10274e0362c3dad9ad6bb
derek ney <deek311@gmail.com>
2022-12-12 03:04:13 -0700

    Revert "test"
    This reverts commit fd75f32443c006175304c8cdfa888fc33847aa39.

--- src/routes/auth/+page.server.js
+++ src/routes/auth/+page.server.js
@@ -1,3 +1,37 @@
+// import { invalid, redirect } from '@sveltejs/kit'
+
+
+// /** @type {import('./$types').Actions} */ 
+// export const actions = {
+//     login: async ({ request }) => {
+//         const data = await request.formData()
+//         const email = data.get('email')
+//         const password = data.get('password')
+        
+//         if (!email) { 
+//             return invalid(400, {email, missing: true})
+//         }
+//         try {
+//             const verifidedUser = await Auth.signin(email, password)
+
+//         }
+//         catch(err) {
+//             if(err.code) {
+//                 return invalid(400, {errorMessage: err.message, invalidLogin: true})
+//             }
+//         }
+        
+// 		throw redirect(303, '/dashboard');
+		
+//     },
+//     signup: async ({ request }) => {
+//         const data = await request.formData()
+//         //connect to online later
+//         throw redirect(307, '/auth/login')
+        
+
+//     }
+// }
 
 
 import { invalid, redirect } from '@sveltejs/kit'

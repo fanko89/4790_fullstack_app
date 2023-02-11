656a0e0f76ebceb7f55af0c0990e9dc30c2c5fe2
derek ney <deek311@gmail.com>
2023-02-10 23:21:11 -0700

    push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "4790backenda2bebd4d": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}

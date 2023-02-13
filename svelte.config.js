dda598c081d27aad02f62e2d2fb867f485cc65f0
derek ney <deek311@gmail.com>
2023-02-13 14:37:58 -0700

    fixed stuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
 	kit: {
-		adapter: adapter({
-			autoDeploy: true,
-			FQDN: 'https://main.d14fmke5xkadk4.amplifyapp.com',
-			stackName:'470_backend'
-
-		})
+		adapter: adapter()
 
 	}
 };
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /**@type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'./runtimeConfig': './runtimeConfig.brower'
+		}
+	}
 	build: {sourcemap: true}
 };
 
